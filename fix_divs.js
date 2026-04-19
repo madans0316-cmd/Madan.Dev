8c50d8e42c09c23dd18f7f3f63b7933eddb71663
Madan Kumar S <madans0316@gmail.com>
2026-04-19 22:39:52 +0530

    Clean up: remove old portfolio files, scripts, and temp data

--- fix_divs.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-let txt = fs.readFileSync('index.html', 'utf-8');
-const newTxt = txt.replace(/(<div class="skill-tab-item">.*?<\/div>)\s*<\/div>/g, '$1');
-if(newTxt !== txt) {
-    fs.writeFileSync('index.html', newTxt);
-    console.log('Fixed extra divs!');
-} else {
-    console.log('No extra divs found.');
-}
